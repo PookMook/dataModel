cef80f0d0347214f46b970f09c2a2eb67d01e2c1
PookMook <arthur.juchereau@gmail.com>
2019-09-29 00:12:12 -0400

    fix typo

--- dynaodb.js
+++ dynaodb.js
@@ -14,6 +14,20 @@ const dynamoose = require('dynamoose');
  
 performance.mark('Module');
 
+
+const createDynamooseInstance = () => {
+    dynamoose.AWS.config.update({
+      accessKeyId: 'env.process.KEY_ID',
+      secretAccessKey: 'env.process.KEY',
+      region: 'env.process.REGION'
+    });
+    //dynamoose.local(); // This defaults to "http://localhost:8000"
+}
+
+createDynamooseInstance();
+
+performance.mark('Config');
+
 // Create cat model with default options
 const Session = dynamoose.model('session', {
   id: Number,
@@ -44,7 +58,8 @@ performance.mark('Fetch');
 
 
 performance.measure('Module', 'Init', 'Module');
-performance.measure('Model', 'Module', 'Model');
+performance.measure('Config', 'Module', 'Config');
+performance.measure('Model', 'Config', 'Model');
 performance.measure('Local', 'Model', 'Local');
 performance.measure('Save', 'Local', 'Save');
 performance.measure('Fetch', 'Save', 'Fetch');
